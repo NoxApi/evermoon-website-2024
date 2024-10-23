e8355765d251ea5a2963cc1d48a97a16ce943a14
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2024-10-23 15:07:48 +0700

    remove loading

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,7 +6,7 @@ import Hero from "../../components/Hero/index";
 import Roadmap from "../../components/Roadmap/index";
 import NavBar from "../../components/Navbar/Index";
 import { Josefin_Sans } from "next/font/google";
-import Loading from "../Loading";
+// import Loading from "../Loading";
 
 const Jose = Josefin_Sans({ subsets: ["latin"] });
 const Index = () => {
@@ -18,59 +18,59 @@ const Index = () => {
   //   }, 1000);
   // }
 
-  function IsLoadScreen() {
-    if (isLoading) {
-      return <Loading />;
-    } else if (!load) {
-      return (
-        <>
-          <Video />
-          <Hover />
-          <Hero />
-          <Partner />
-          <Roadmap />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <Video />
-          <Hover />
-          <Hero />
-        </>
-      );
-    }
-  }
+  // function IsLoadScreen() {
+  //   if (isLoading) {
+  //     return <Loading />;
+  //   } else if (!load) {
+  //     return (
+  //       <>
+  //         <Video />
+  //         <Hover />
+  //         <Hero />
+  //         <Partner />
+  //         <Roadmap />
+  //       </>
+  //     );
+  //   } else {
+  //     return (
+  //       <>
+  //         <Video />
+  //         <Hover />
+  //         <Hero />
+  //       </>
+  //     );
+  //   }
+  // }
 
-  useEffect(() => {
-    const handleLoad = () => {
-      setIsLoading(false);
-      setTimeout(() => {
-        setLoad(false);
-      }, 5000);
-      // alert("Your page is loaded");
-    };
+  // useEffect(() => {
+  //   const handleLoad = () => {
+  //     setIsLoading(false);
+  //     setTimeout(() => {
+  //       setLoad(false);
+  //     }, 5000);
+  //     // alert("Your page is loaded");
+  //   };
 
-    if (document.readyState === "complete") {
-      handleLoad(); // Call immediately if the page is already loaded
-    } else {
-      window.addEventListener("load", handleLoad);
-      // method attaches an event handler to a window
-    }
+  //   if (document.readyState === "complete") {
+  //     handleLoad(); // Call immediately if the page is already loaded
+  //   } else {
+  //     window.addEventListener("load", handleLoad);
+  //     // method attaches an event handler to a window
+  //   }
 
-    // Cleanup the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("load", handleLoad);
-    };
-  }, []);
+  //   // Cleanup the event listener when the component unmounts
+  //   return () => {
+  //     window.removeEventListener("load", handleLoad);
+  //   };
+  // }, []);
 
   // const timing = performance.getEntriesByType("navigation")[0];
 
   // console.log(timing);
 
-  if (load) {
-    return <Loading />;
-  }
+  // if (load) {
+  //   return <Loading />;
+  // }
 
   // const [isfullscreen, setisfullscreen] = useState(false);
   //   const [isloaded, setisloaded] = useState(false);
@@ -89,8 +89,12 @@ const Index = () => {
         className={` flex w-full flex-col items-center bg-gradient-to-r from-[#000B1A] to-[#00112C] text-[#F1E3B5] ${Jose.className} `}
       >
         <NavBar />
-
-        {IsLoadScreen()}
+        <Video />
+        <Hover />
+        <Hero />
+        <Partner />
+        <Roadmap />
+        {/* {IsLoadScreen()} */}
       </div>
     </>
   );
